3e657119bb7340c412a76073b6212a3480324e2a
LeaninusSplendidus <91956716+LeaninusSplendidus@users.noreply.github.com>
2021-10-26 13:24:44 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+function introduceMe (name: string, age: number) {
+    return "hi my name is " + name + "and my age is " + age
+}
+let introduction = introduceMe("peter", 42)
+console.log(introduction)
