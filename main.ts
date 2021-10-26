16ae329dfe499ea2cd23359f14da90436385d027
LeaninusSplendidus <91956716+LeaninusSplendidus@users.noreply.github.com>
2021-10-26 21:35:47 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
+
 function introduceMe (name: string, age: number) {
     return "hi my name is " + name + "and my age is " + age
 }
 let introduction = introduceMe("peter", 42)
 console.log(introduction)
+
+let list = ["James", "Peter", "Jakob", "Stine"]
+
+for (let i = 0; i < list.length; i++) {
+    console.log(list[i])
+}
